a20cd676ed418c17f5ca0f8b905b69c662c13f3b
abdul-kadir-uk <abdulkadiruk06@gmail.com>
2024-06-29 16:29:07 +0530

    build the structure of the project

---
+++ script.js
@@ -0,0 +1,10 @@
+api_key = "bbd443ceafd641435a14ff18d36acdba";
+
+const search_input = document.getElementById("city_input");
+const search_btn = document.getElementById("Search_btn");
+const location_btn = document.getElementById("location_btn");
+
+
+
+
+
